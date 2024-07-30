53f4306d13df565133790e3a584bbbf91b7e40ce
Mohammed Zaid <mohammedzee98@gmail.com>
2024-07-31 00:57:20 +0530

    added more comments #foreach() method 03-Array.js

--- week-1/offline-class-1/level-1/03-Array.js
+++ week-1/offline-class-1/level-1/03-Array.js
@@ -51,6 +51,7 @@ function concatExample(arr1, arr2) {
 concatExample([1, 2, 3], [4, 5, 6]);
 
 // forEach()
+// iterates for each of the arr or elements given before the for each keyword.
 function forEachExample(arr) {
   console.log("Original Array:", arr);
 
